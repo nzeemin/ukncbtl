d8bda9e22aaa31f272059ec685e5067c2b4bfc82
nzeemin <nzeemin@gmail.com>
2015-03-22 12:45:07 +0300

    Version number calculation adopted for Git.

---
+++ !version.bat
@@ -0,0 +1,7 @@
+@echo off
+for /f %%i in ('git rev-list HEAD --count') do set REVISION=%%i
+echo The revision is %REVISION%
+echo. > Version.h
+echo #define SVN_REVISION %REVISION% >> Version.h
+echo. >> Version.h
+echo #define UKNCBTL_VERSION_STRING "1.0.%REVISION%" >> Version.h
--- Version.tpl
+++
@@ -1,8 +0,0 @@
-
-#define SVN_REVISION $WCREV$
-
-#define SVN_URL_STRING "$WCURL$"
-
-#define UKNCBTL_VERSION_STRING "1.0.$WCREV$"
-
-
