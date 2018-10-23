cd640761a3e1cb3cea65633967185e2cb88b101d
nzeemin <nzeemin@gmail.com>
2018-10-23 22:51:35 +0300

    Main.cpp: more command-line options.

--- Main.cpp
+++ Main.cpp
@@ -208,10 +208,35 @@ void ParseCommandLine()
     for (int curargn = 1; curargn < argnum; curargn++)
     {
         LPTSTR arg = args[curargn];
+
         if (_tcscmp(arg, _T("/boot")) == 0)
         {
             //TODO: Check if we have Floppy0 image assigned
             Option_AutoBoot = TRUE;
+        }
+        else if (_tcscmp(arg, _T("/autostart")) == 0 || _tcscmp(arg, _T("/autostarton")) == 0)
+        {
+            Settings_SetAutostart(TRUE);
+        }
+        else if (_tcscmp(arg, _T("/autostartoff")) == 0)
+        {
+            Settings_SetAutostart(FALSE);
+        }
+        else if (_tcscmp(arg, _T("/debug")) == 0 || _tcscmp(arg, _T("/debugon")) == 0 || _tcscmp(arg, _T("/debugger")) == 0)
+        {
+            Settings_SetDebug(TRUE);
+        }
+        else if (_tcscmp(arg, _T("/debugoff")) == 0)
+        {
+            Settings_SetDebug(FALSE);
+        }
+        else if (_tcscmp(arg, _T("/sound")) == 0 || _tcscmp(arg, _T("/soundon")) == 0)
+        {
+            Settings_SetSound(TRUE);
+        }
+        else if (_tcscmp(arg, _T("/soundoff")) == 0)
+        {
+            Settings_SetSound(FALSE);
         }
     }
 
